1c2ef7161d3e1a2e27b8d9d2db85c9d0b3f13ab8
aliciasavelly <alicia.savelly@gmail.com>
2017-07-06 17:28:28 -0700

    initial commit

---
+++ python_intro.py
@@ -0,0 +1,15 @@
+if 3 > 2:
+    print("hello!")
+elif 100 < 200:
+    print("elif")
+else:
+    print("it's false")
+
+def hi(name):
+    print("hi " + name)
+
+hi("Alicia")
+hi("Sresht")
+
+for i in range(1, 6):
+    print(i)
