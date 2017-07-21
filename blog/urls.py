b141c209167f7d87fe634864842c4add3ff469be
aliciasavelly <alicia.savelly@gmail.com>
2017-07-20 17:41:41 -0700

    clean up html and work on auth

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import include,url
 from . import views
+from django.contrib import admin
+from django.contrib.auth.views import login
+from django.contrib.auth.views import logout
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
@@ -9,4 +12,8 @@ urlpatterns = [
     url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
     url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
     url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', login, name='login'),
+    url(r'^accounts/logout/$', logout, name='logout', kwargs={'next_page': '/'}),
+    # url(r'', include('blog.urls')),
 ]
